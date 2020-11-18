62c56e8d3a390d0fc43aa941379731a799fd3d19
DomicidalDesigns <dodson.dominic@gmail.com>
2020-11-17 20:54:44 -0500

    Much cleaner existence testing rather than multiple try/except blocks

--- DISrssCORD.py
+++ DISrssCORD.py
@@ -10,74 +10,98 @@ import yaml
 load_dotenv()
 
 webhook_id = os.getenv('WEBHOOK_ID')
-
-wait = 60  # seconds
 webhook = f"https://discordapp.com/api/webhooks/{webhook_id}"
+
+frequency = {}
+frequency['update'] = 60  # seconds - checking for new posts
+frequency['post'] = 10  # seconds - posting between each feed
+
+# DEBUG
+DEBUG = False
+
+if DEBUG:
+    frequency['update'] = 1
+    frequency['post'] = 1
 
 
 def RSSparser(url):
+    '''Fetches newest RSS article and posts it to a discord webhook url.
+    '''
     rss = feedparser.parse(url)
-
+    # Most recent article
     u = rss.entries[0]
+
+    rsstitle = rss.feed.get('title', 'No title')
 
     current_time = time.time()
     current_timestamp = datetime.fromtimestamp(
         current_time
     ).strftime("%A, %B %d, %Y %I:%M:%S")
 
-    # Set default values for feed attributes
+    # Declare feed dictionary and check for feed elements.
+    # This uses the python dictionary get() method to set a value if
+    # the element does not exist
+
     feed = {
-        'link': None,
-        'date': current_timestamp,
-        'icon':
-            'https://live.staticflickr.com/3777/10813661054_709581b384_b.jpg',
-        'title': None,
-        'desc': None,
-        'content': None,
-        'img': None
+        'link':
+            u.get('link', 'No link (!)'),
+        'date':
+            u.get('published', u.get('updated', current_timestamp)),
+        'icon': rss.feed.get(
+            'icon',
+            'https://live.staticflickr.com/3777/10813661054_709581b384_b.jpg'
+            ),
+        'title':
+            u.get('title', 'No title'),
+        'desc':
+            u.get('summary', 'No description'),
+        'content':
+            u.get('content', 'No content'),
     }
 
-    # Try updating feed attributes
-    try:
-        feed['link'] = u.link
-        feed['date'] = u.published
-        feed['icon'] = rss.feed.image.href
-        feed['title'] = u.title
-        feed['desc'] = u.summary
-        feed['content'] = u.content
-        feed['img'] = rss.media_thumbnail[0].url
-
-    except AttributeError as e:
-        print(f'[MISSING] Feed {e}\n')
-        # print(feed)
+    # TODO: feed['image']
 
     payload = {
-        'username': rss.feed.title,
-        'content': f"**Date:**  ` {feed['date']} ` \n {feed['link']} \n\n ",
-        'avatar_url': feed['icon']
+        'username':
+            # This sets the webhook bot's username to the post title.
+            feed['title'].split('. ')[0],
+        'content':
+            # This is the message sent to the discord channel (Markdown works).
+            f"{feed['link']} \n",
+        'avatar_url':
+            # Change this link to customize the webhook bot's avatar picture.
+            'https://live.staticflickr.com/3777/10813661054_709581b384_b.jpg'
     }
 
-    print(f"{rss.feed.title}")
+    print(f"{rsstitle}")
+    # Get newest article link and compare it to the one saved in the YAML file
     if u.link != urls[url]:
         print(
             "[NEW ARTICLE] "
             "Posting new article: "
-            f"'{u.title}' to feed '{rss.feed.title}'"
+            f"'{feed['title']}' to feed '{rsstitle}'"
         )
         urls[url] = u.link
-        requests.post(webhook, data=payload)
+        post = requests.post(webhook, data=payload)
+        # DEBUG:
+        if DEBUG:
+            print(post, feed['link'], '\n')
+            print(payload)
 
         # Update YAML file with new article title
         with open('rss_urls.yaml', 'w') as rss_urls:
             yaml.dump(urls, rss_urls)
+
     else:
-        print(f"[CURRENT] '{rss.feed.title}' is up to date")
+        print(f"[CURRENT] '{rsstitle}' is up to date")
+
+    time.sleep(frequency['post'])
 
 
 while True:
     with open('rss_urls.yaml') as rss_urls:
         urls = yaml.load(rss_urls, Loader=yaml.FullLoader)
 
-    for url in list(urls.keys()):
+    for url in urls.keys():
         RSSparser(url)
-    time.sleep(wait)
+    time.sleep(frequency['update'])
